d86acad25afb6a6c65af7ec10e4d14ec434db451
koppe555 <kohei.ishibashi55@gmail.com>
2022-02-01 01:03:08 +0900

    chore(routes): ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users', to: 'users#index'
+  get '/', to: 'users#index'
   get 'search_result', to: 'users#index'
   post 'search_result', to: 'users#search_result'
 end
